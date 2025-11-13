77323a34e1346b140cfde3d198ba1fe57f2d6833
MaximusDiplodocus <desjobertmaxime1@gmail.com>
2025-11-13 10:50:09 +0100

    Update_roles

--- src/roles.jl
+++ src/roles.jl
@@ -1,3 +1,4 @@
+# Définition d'une structure Stats avec des champs modifiables
 mutable struct Stats
     PV::Int
     PM::Int
@@ -5,17 +6,23 @@ mutable struct Stats
     VITESSE::Int
     DEFENSE::Int
 end
+
+# Constructeur secondaire pour définir des valeurs par défault pour Stats
 Stats(; PV=100, PM=50, ATK=10, VITESSE=10, DEFENSE=10) = Stats(PV, PM, ATK, VITESSE, DEFENSE)
 
+# Définition d'un type abstrait pour la hiérarchie des rôles
 abstract type Role end
 
+# Déclaration du rôle Archer, qui hérite de Role
 mutable struct Archer <: Role
     nom::String
     stats::Stats
     effects::Vector{Any}
 end
+# Constructeur secondaire pour définir des valeurs par défaut pour Archer
 Archer(; nom="Robin") = Archer(nom, Stats(PV=90, PM=30, ATK=15, VITESSE=20, DEFENSE=8), [])
 
+# Déclaration du rôle Mage
 mutable struct Mage <: Role
     nom::String
     stats::Stats
@@ -23,6 +30,7 @@ mutable struct Mage <: Role
 end
 Mage(; nom="Gandalf") = Mage(nom, Stats(PV=70, PM=100, ATK=25, VITESSE=12, DEFENSE=6), [])
 
+# Déclaration du rôle Chevalier
 mutable struct Chevalier <: Role
     nom::String
     stats::Stats
@@ -30,6 +38,7 @@ mutable struct Chevalier <: Role
 end
 Chevalier(; nom="Arthur") = Chevalier(nom, Stats(PV=120, PM=20, ATK=18, VITESSE=10, DEFENSE=20), [])
 
+# Déclaration du rôle Gobelin
 mutable struct Gobelin <: Role
     nom::String
     stats::Stats
@@ -37,9 +46,10 @@ mutable struct Gobelin <: Role
 end
 Gobelin(; nom="Gabimarou") = Gobelin(nom, Stats(PV=60, PM=10, ATK=12, VITESSE=14, DEFENSE=5), [])
 
+# Affichage spécifique pour Role
 function Base.show(io::IO, perso::Role)
-    cls = typeof(perso).name.name
+    rl = typeof(perso).name.name
     st = perso.stats
-    println(io, "$(perso.nom) le $cls")
+    println(io, "$(perso.nom) le $rl")
     println(io, "  PV: $(st.PV) | PM: $(st.PM) | ATK: $(st.ATK) | VIT: $(st.VITESSE) | DEF: $(st.DEFENSE)")
 end
